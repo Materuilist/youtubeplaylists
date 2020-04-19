628d652544f606f4a41db5ba6fe061a537bf846f
Kirill <kirpetrosian@mail.ru>
2020-04-19 17:29:01 +0500

    linked redux to search input component

--- src/components/Layout/PlaylistSearcher/Search/Search.js
+++ src/components/Layout/PlaylistSearcher/Search/Search.js
@@ -1,14 +1,40 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
+import { connect } from "react-redux";
+
+import { fetchPlaylists } from "../../../../redux/actions/creators";
 
 import cssClasses from "./Search.module.css";
 
-export default function () {
-    const searchInput = useRef();
+const INPUT_DELAY = 1000;
+
+function Search({ fetchPlaylists }) {
+  const searchInput = useRef();
+
+  const inputHandler = async (event) => {
+    const playlistName = event.target.value;
+    await new Promise((resolve)=>setTimeout(()=>resolve(), INPUT_DELAY));
+    fetchPlaylists(playlistName);
+  };
 
   return (
-    <div onClick={()=>searchInput.current.focus()} className={cssClasses.Search}>
+    <div
+      onClick={() => searchInput.current.focus()}
+      className={cssClasses.Search}
+    >
       <span className={cssClasses.SearchIcon}>&#9906;</span>{" "}
-      <input ref={searchInput} type="" className={cssClasses.SearchInput} placeholder={`enter playlist's name...`}/>
+      <input
+        onInput={inputHandler}
+        ref={searchInput}
+        type="text"
+        className={cssClasses.SearchInput}
+        placeholder={`enter playlist's name...`}
+      />
     </div>
   );
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchPlaylists: (playlistName) => dispatch(fetchPlaylists(playlistName)),
+});
+
+export default connect(null, mapDispatchToProps)(Search);
--- src/redux/actions/creators.js
+++ src/redux/actions/creators.js
@@ -1,25 +1,50 @@
-import {SET_PLAYLIST, TOGGLE_LOADING, SET_DATE} from './types';
+import {
+  SET_PLAYLIST,
+  TOGGLE_LOADING,
+  SET_DATE,
+  SET_SEARCH_RESULTS,
+} from "./types";
 
-export function setPlaylist(playlistName){
-    return({
-        type:SET_PLAYLIST,
-        playlistName
-    })
+const BACKEND_WITH_KEY =
+  "https://www.googleapis.com/youtube/v3/playlists?key=AIzaSyDAhOBa7cZ-KrkW2hAWoBsOBVqVBZ52lHo&part=snippet";
+
+export function setPlaylist(playlistName) {
+  return {
+    type: SET_PLAYLIST,
+    playlistName,
+  };
 }
-export function toggleLoading(){
-    return({
-        type:TOGGLE_LOADING
-    })
+export function toggleLoading() {
+  return {
+    type: TOGGLE_LOADING,
+  };
 }
-export function setDate(){
-    return({
-        type:SET_DATE,
-        date:Date.now()
-    })
+export function setDate() {
+  return {
+    type: SET_DATE,
+    date: Date.now(),
+  };
 }
 
-export function fetchPlaylist(playlistName){
-    return async (dispatch, getState)=>{
-        
+export function setSearchResults(searchResults) {
+  return {
+    type: SET_SEARCH_RESULTS,
+    searchResults,
+  };
+}
+
+export function fetchPlaylists(playlistName) {
+  return async (dispatch, getState) => {
+    dispatch(setDate());
+    const { loading } = getState().searchResults;
+    if (!loading) {
+      dispatch(toggleLoading());
     }
+    const res = await fetch(
+      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDAhOBa7cZ-KrkW2hAWoBsOBVqVBZ52lHo&part=snippet&type=playlist&order=viewCount&maxResults=7&q=" +
+        playlistName
+    );
+    const searchResults = await res.json();
+    dispatch(setSearchResults(searchResults.items));
+  };
 }
